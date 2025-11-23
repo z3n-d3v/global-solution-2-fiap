22b8362fc85fbf50bed52febf5244b272b03e990
JuJuVitoria <juliavitoriadaluz5@gmail.com>
2025-11-22 23:07:43 -0300

    feat: adiciona estrutura inicial de layout para o app

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -1,5 +1,5 @@
 import { GraduationCap } from "lucide-react";
-import { Button } from "./ui/button";
+import { Button } from "../../components/ui/button";
 
 interface HeaderProps {
   isAuthenticated?: boolean;
