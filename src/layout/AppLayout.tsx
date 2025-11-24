a1c9ccf4c035d854f15f05d24d3fd044ee1e686f
BL4CKM4G3 <alvaro_rangel@outlook.com>
2025-11-23 22:06:09 -0300

    fix more stuff

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/AuthContext";
 import {
     LayoutDashboard,
     Target,
@@ -5,9 +7,13 @@ import {
     Award,
     TrendingUp,
     GraduationCap,
-    BookMarked
+    BookMarked,
+    ArrowLeft,
+    ArrowRight
 } from "lucide-react";
 import { Link, Outlet } from "react-router-dom";
+
+import { useState } from "react"
 
 const menuItems = [
     { icon: LayoutDashboard, label: "Dashboard", active: true, path: "/dashboard" },
@@ -18,39 +24,62 @@ const menuItems = [
     { icon: BookMarked, label: "Meu Currículo", active: false, path: "/resume-review" },
 ];
 
+
 export const AppLayout = () => {
+    const { logout } = useAuth();
+    const [collapsed, setCollapsed] = useState(false);
+
+    const toggle = () => setCollapsed(!collapsed);
+
     return (
-        <div className="relative z-0 min-h-screen bg-background">
+        <div className="relative z-0 max-h-screen bg-background">
             <div className="flex">
                 {/* Sidebar */}
-                <aside className="w-64 border-r bg-card min-h-screen p-6 hidden lg:block">
+                <aside className={`${collapsed ? "w-32" : "w-64"} border-r bg-card min-h-screen max-h-screen p-6 hidden lg:block`}>
                     <div
-                        className="flex items-center gap-2 cursor-pointer mb-4"
+                        className={`flex ${collapsed ? "flex-col items-center" : "flex-row w-fit items-center"} just gap-2 cursor-pointer mb-4`}
+                        onClick={toggle}
+                        title={`Clique para ${collapsed ? "Abrir" : "Fechar"}`}
                     >
-                        <div className="bg-primary p-2 rounded-lg">
+                        <div className="bg-primary p-2 rounded-lg w-fit">
                             <GraduationCap className="h-6 w-6 text-primary-foreground" />
                         </div>
                         <span className="text-primary">TrilhaPro</span>
                     </div>
-                    <div className="space-y-1">
+                    <div className="flex flex-col">
+
+                    </div>
+                    <div className="space-y-1 flex flex-col items-center">
                         {menuItems.map((item, index) => (
                             <Link
                                 key={index}
-                                className={`w-full flex items-center gap-3 px-4 py-3 rounded-lg transition-colors ${item.active
+                                className={`${collapsed ? "w-fit" : "w-full"} flex items-center gap-3 px-4 py-3 rounded-lg transition-colors ${item.active
                                     ? 'bg-primary text-primary-foreground'
                                     : 'hover:bg-accent text-foreground'
                                     }`}
                                 to={item.path}
                             >
-                                <item.icon className="h-5 w-5" />
-                                <span>{item.label}</span>
+                                {collapsed ? (
+                                    <>
+                                        <item.icon className="h-5 w-5" />
+                                    </>
+                                ) : (
+                                    <>
+                                        <item.icon className="h-5 w-5" />
+                                        <span>{item.label}</span>
+                                    </>
+                                )}
                             </Link>
                         ))}
+                    </div>
+
+                    <div className="h-fit flex flex-col-reverse">
+                        <Button onClick={() => { logout() }}>Logout</Button>
                     </div>
                 </aside>
 
                 {/* Main Content */}
-                <main className="flex-1 p-6 lg:p-8 relative z-auto">
+                <main className="flex-1 p-6 lg:p-8 relative z-auto max-h-screen overflow-scroll">
                     <Outlet />
                 </main>
             </div>
